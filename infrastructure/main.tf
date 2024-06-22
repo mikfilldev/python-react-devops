eb98b4e87641ef87cd7161d3aa68168b8ae883bb
Mykola Filippenko <mykolafilippenkodev@gmail.com>
2024-06-22 20:08:38 +0200

    formatting tf files

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,6 +3,6 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "resource_group" {
-  name      = var.AZURE_RESOURCE_GROUP_NAME
-  location  = var.AZURE_RESOURCE_GROUP_LOCATION
+  name     = var.AZURE_RESOURCE_GROUP_NAME
+  location = var.AZURE_RESOURCE_GROUP_LOCATION
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,8 +1,8 @@
 variable "AZURE_RESOURCE_GROUP_NAME" {
   default = "Is not variable there"
-  type        = string
+  type    = string
 }
 variable "AZURE_RESOURCE_GROUP_LOCATION" {
   default = "Is not variable there"
-  type        = string
+  type    = string
 }
